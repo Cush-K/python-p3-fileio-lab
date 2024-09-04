f36bc100bf067ff2ace621604584b289ce2834ec
Kalvine <kkalvine4@gmail.com>
2024-09-04 15:16:19 +0300

    File handling

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
-def write_file(file_name, file_content):
-    pass
+def write_file(file_name = "test_file", file_content="This is a test content."):
+    with open(file_name, mode = 'w', encoding='utf-8') as test_file:
+        test_file.write(file_content)
 
-def append_file(file_name, append_content):
-    pass
+def append_file(file_name = "test_file", append_content="\nAppended content."):
+    with open(file_name, mode = 'a', encoding='utf-8') as test_file:
+        test_file.write(append_content)
 
-def read_file(file_name):
-    pass
+def read_file(file_name = "test_file"):
+    with open(file_name, encoding='utf-8') as test_file:
+        return test_file.read()
+
+write_file("files/test_file.txt")
+append_file("files/test_file.txt")
+content = read_file("files/test_file.txt")
+print(content)
